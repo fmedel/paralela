0f2a81641b4e82968328f03ad5813d1697187d01
fmedel <felipemedelatorre@hotmail.com>
2017-10-13 20:23:33 -0300

    modificacion de las mac

--- prender.sh
+++ prender.sh
@@ -23,9 +23,8 @@ do
   echo "11)Prender P4"
   echo "12)Prender P5"
   echo "13)Prender P6"
-  echo "14)Prender P11"
-  echo "15)Prender P12"
-
+  echo "14)Prender P1X"
+  echo "15)Prender P1X"
   echo "----------------------"
   echo "21)conectar al pc de felipe"
   echo "22)conectar al pc de diego"
@@ -33,7 +32,17 @@ do
   echo "24)conectar al pc de leo"
   echo "25)conectar al pc reyes"
   echo "26)conectar al pc francisco"
-  echo "26)conectar al pc Fabian"
+  echo "27)conectar al pc Fabian"
+  echo " "
+  echo "28)conectar al pc P1"
+  echo "29)conectar al pc P2"
+  echo "30)conectar al pc P3"
+  echo "31)conectar al pc P4"
+  echo "32)conectar al pc P5"
+  echo "33)conectar al pc P6"
+  echo "34)conectar al pc P1X"
+  echo "35)conectar al pc P1X"
+  echo "----------------------"
   echo "99) salir"
   read x
   case $x in
@@ -57,7 +66,67 @@ do
       echo "encendido ip 10.1.10.121"
       sleep 40
       ;;
-
+      8)
+      #no tiene puertos  o no funciona
+      wakeonlan ac:16:2d:06:3a:7f
+      echo "encendido ip 10.1.10.??"
+      sleep 40
+      ;;
+      9)
+      #no tiene puertos  o no funciona
+      wakeonlan 24:be:05:07:a6:4a
+      echo "encendido ip 10.1.10.??"
+      sleep 40
+      ;;
+      10)
+      #no tiene puertos  o no funciona
+      wakeonlan ac:16:2d:11:c7:9c
+      echo "encendido ip 10.1.10.??"
+      sleep 40
+      ;;
+      11)
+      #no tiene puertos  o no funciona
+      wakeonlan ac:16:2d:06:3a:94
+      echo "encendido ip 10.1.10.??"
+      sleep 40
+      ;;
+      12)
+      #no tiene puertos  o no funciona
+      wakeonlan  ac:16:2d:11:c9:54
+      echo "encendido ip 10.1.10.??"
+      sleep 40
+      ;;
+      13)
+      #no tiene puertos  o no funciona
+      wakeonlan ac:16:2d:0d:da:fb
+      echo "encendido ip 10.1.10.??"
+      sleep 40
+      ;;
+      14)
+      #no tiene puertos  o no funciona
+      wakeonlan ac:16:2d:0d:da:f3
+      echo "encendido ip 10.1.10.??"
+      sleep 40
+      ;;
+      15)
+      #no tiene puertos  o no funciona
+      wakeonlan ac:16:2d:06:3a:6e
+      echo "encendido ip 10.1.10.??"
+      sleep 40
+      ;;
+      16)
+      #no tiene puertos  o no funciona
+      wakeonlan ac:16:2d:0e:58:77
+      echo "encendido ip 10.1.10.??"
+      sleep 40
+      ;;
+      17)
+      #no tiene puertos  o no funciona
+      wakeonlan ac:16:2d:11:c9:51
+      echo "encendido ip 10.1.10.??"
+      sleep 40
+      ;;
+      ##########################################################################
       21)
       echo "ip 10.1.10.124"
       ssh paralela_nueva@10.1.10.124
@@ -74,7 +143,6 @@ do
       echo "ip 10.1.10.121"
       ssh paralela@10.1.10.121
       ;;
-
      99)
         exit
      ;;
