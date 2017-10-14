78647e3ede91a3a5b31f4e74b93022012eb392cd
fmedel <felipemedelatorre@hotmail.com>
2017-10-14 17:55:15 -0300

    completo prender.sh

--- prender.sh
+++ prender.sh
@@ -4,28 +4,25 @@ while [ $plantilla -le 1 ]
 do
   clear
   echo "---------------------"
-  echo "|P1 |P2 |P3 |P4 |P5 |"
-  echo "|P6 |P7 |P8 |P9 |P10|"
-  echo "|P11|P12|P13|P14|P15|"
+  echo "pc-profesor"
+  echo "|P1 |P2x |P3 |P4x|"
+  echo "|P6x |P7 |P8 |P9 |P10|"
+  echo "|P11|P12|P13|P14x|P15x|"
   echo "---------------------"
   echo " "
+  echo "0)Prender pc porfesor"
   echo "1)Prender P9 de felipe"
   echo "2)Prender P8 de diego"
   echo "3)Prender P10 de ignacio"
   echo "4)Prender P7 de leo"
-  echo "5)Prender pc reyes"
-  echo "6)Prender pc francisco"
-  echo "7)Prender pc Fabian"
+  echo "5)Prender P13 reyes"
+  echo "6)Prender P12 francisco"
+  echo "7)Prender P11 Fabian"
   echo " "
   echo "8)Prender P1"
-  echo "9)Prender P2"
-  echo "10)Prender P3"
-  echo "11)Prender P4"
-  echo "12)Prender P5"
-  echo "13)Prender P6"
-  echo "14)Prender P1X"
-  echo "15)Prender P1X"
+  echo "9)Prender P3"
   echo "----------------------"
+  echo "20)conectar al pc del profesor"
   echo "21)conectar al pc de felipe"
   echo "22)conectar al pc de diego"
   echo "23)conectar al pc de ignacio"
@@ -35,17 +32,16 @@ do
   echo "27)conectar al pc Fabian"
   echo " "
   echo "28)conectar al pc P1"
-  echo "29)conectar al pc P2"
-  echo "30)conectar al pc P3"
-  echo "31)conectar al pc P4"
-  echo "32)conectar al pc P5"
-  echo "33)conectar al pc P6"
-  echo "34)conectar al pc P1X"
-  echo "35)conectar al pc P1X"
+  echo "29)conectar al pc P3"
   echo "----------------------"
   echo "99) salir"
   read x
   case $x in
+      0)
+      wakeonlan 4c:72:b9:b1:92:f2
+      echo "encendido  ip 10.1.10.115"
+      sleep 40
+      ;;
       1)
       wakeonlan ac:16:2d:06:3a:7c
       echo "encendido  ip 10.1.10.124"
@@ -66,82 +62,69 @@ do
       echo "encendido ip 10.1.10.121"
       sleep 40
       ;;
+      5)
+      wakeonlan 24:be:05:07:a6:4a
+      echo "encendido ip 10.1.10.127"
+      echo "paralelanueva"
+      echo "distribuida"
+      sleep 40
+      ;;
+      6)
+      wakeonlan ac:16:2d:11:c9:51
+      echo "encendido ip 10.1.10.126"
+      echo "paralelavieja"
+      echo "peramaxima"
+      sleep 40
+      ;;
+      7)
+      wakeonlan ac:16:2d:06:3a:6e
+      echo "encendido ip 10.1.10.123"
+      echo "paralelavieja"
+      echo "peramaxima"
+      sleep 40
+      ;;
       8)
       #no tiene puertos  o no funciona
       wakeonlan ac:16:2d:06:3a:7f
-      echo "encendido ip 10.1.10.??"
+      echo "encendido ip 10.1.10.116"
+      echo "paralelanueva"
+      echo "distribuida"
       sleep 40
       ;;
       9)
-      #no tiene puertos  o no funciona
-      wakeonlan 24:be:05:07:a6:4a
-      echo "encendido ip 10.1.10.??"
-      sleep 40
-      ;;
-      10)
-      #no tiene puertos  o no funciona
-      wakeonlan ac:16:2d:11:c7:9c
-      echo "encendido ip 10.1.10.??"
-      sleep 40
-      ;;
-      11)
-      #no tiene puertos  o no funciona
-      wakeonlan ac:16:2d:06:3a:94
-      echo "encendido ip 10.1.10.??"
-      sleep 40
-      ;;
-      12)
-      #no tiene puertos  o no funciona
-      wakeonlan  ac:16:2d:11:c9:54
-      echo "encendido ip 10.1.10.??"
-      sleep 40
-      ;;
-      13)
-      #no tiene puertos  o no funciona
-      wakeonlan ac:16:2d:0d:da:fb
-      echo "encendido ip 10.1.10.??"
-      sleep 40
-      ;;
-      14)
-      #no tiene puertos  o no funciona
-      wakeonlan ac:16:2d:0d:da:f3
-      echo "encendido ip 10.1.10.??"
-      sleep 40
-      ;;
-      15)
-      #no tiene puertos  o no funciona
-      wakeonlan ac:16:2d:06:3a:6e
-      echo "encendido ip 10.1.10.??"
-      sleep 40
-      ;;
-      16)
-      #no tiene puertos  o no funciona
       wakeonlan ac:16:2d:0e:58:77
-      echo "encendido ip 10.1.10.??"
-      sleep 40
-      ;;
-      17)
-      #no tiene puertos  o no funciona
-      wakeonlan ac:16:2d:11:c9:51
-      echo "encendido ip 10.1.10.??"
+      echo "encendido ip 10.1.10.118"
+      echo "paralelanueva"
+      echo "distribuida"
       sleep 40
       ;;
       ##########################################################################
       21)
-      echo "ip 10.1.10.124"
       ssh paralela_nueva@10.1.10.124
       ;;
       22)
-      echo "ip 10.1.10.122"
       ssh paralela_nueva@10.1.10.122
       ;;
       23)
-      echo "ip 10.1.10.125"
       ssh paralela_nueva@10.1.10.125
       ;;
       24)
-      echo "ip 10.1.10.121"
-      ssh paralela@10.1.10.121
+      ssh paralela_nueva@10.1.10.121
+      ;;
+      25)
+      ssh xxxxxx@10.1.10.127 #tiene q ser mismo user en 25 26 27
+      ;;
+      26)
+      ssh xxxxxx@10.1.10.126 #tiene q ser mismo user en 25 26 27
+      ;;
+      27)
+      ssh xxxxxxx@10.1.10.123 #tiene q ser mismo user en 25 26 27
+      ;;
+      28)
+      ssh paralela_nueva@10.1.10.116 #aun no se configua
+      ;;
+      29)
+      ssh paralela_nueva@10.1.10.118 #aun no se configura
       ;;
      99)
         exit
